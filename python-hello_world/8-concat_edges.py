9e15ad7dff76af1350d660446f61582b0d4d1f38
us3rgnv <you@example.com>
2025-11-16 18:52:35 +0000

    Add all current changes

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
 # This script prints "object-oriented programming with Python" without string literals
 
 str1 = "object-oriented "
 str2 = "programming with Python"
-print(str1 + str2)

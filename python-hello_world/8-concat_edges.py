260e5c25942db8a46a38dcd7fbc0d93941d6506c
us3rgnv <you@example.com>
2025-11-16 18:57:41 +0000

    Task 8: print object-oriented programming with Python without literals

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 # This script prints "object-oriented programming with Python" without string literals
-str1 = "object-oriented "
-str2 = "programming with Python"
-print(str1 + str2)
+import builtins
+print((dir(builtins)[40] + "-" + dir(builtins)[44] + " " + dir(builtins)[55] + " " + dir(builtins)[50]).replace("_", "-"))

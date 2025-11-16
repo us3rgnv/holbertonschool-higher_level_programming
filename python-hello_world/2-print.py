0fdf26ce4ba7ba9ad7627ef6271858bd0070e482
us3rgnv <you@example.com>
2025-11-16 18:24:59 +0000

    Add task 2-print.py

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-# This script prints a message about programming
-
-print("Programming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle")

cc9edeb9c4a1fb3484a1ba301db4396f2702d253
us3rgnv <you@example.com>
2025-11-23 14:25:05 +0000

    Fix 1-search_replace.py to comply with pycodestyle

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """Return a new list with all occurrences of 'search' replaced by 'replace'"""
-    return [replace if x == search else x for x in my_list]
+    new_list = [replace if x == search else x for x in my_list]
+    return new_list

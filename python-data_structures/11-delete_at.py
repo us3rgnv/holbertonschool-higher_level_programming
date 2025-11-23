b9f685626288969782f83337e76c9e510f137fdb
us3rgnv <you@example.com>
2025-11-23 13:42:00 +0000

    Add function that deletes an item at a specific index in a list

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """Delete the item at a specific position in a list."""
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    return my_list[:idx] + my_list[idx+1:]

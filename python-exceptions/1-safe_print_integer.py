1f984b5271d703279a554f2b95fe458803cbb232
us3rgnv <you@example.com>
2025-11-23 15:51:32 +0000

    Solve 1-safe_print_integer task

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """Print an integer using {:d}. Return True if successful, False otherwise."""
+    """Print an integer using {:d}.
+    Return True if successful, False otherwise.
+    """
     try:
         print("{:d}".format(value))
         return True

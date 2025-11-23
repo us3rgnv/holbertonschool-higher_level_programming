4b0f995b567a6a4c83a58b050c8faf462855a7a8
us3rgnv <you@example.com>
2025-11-23 13:36:21 +0000

    Add function that returns the biggest integer in a list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """Return the biggest integer in a list."""
+    if not my_list:
+        return None
+    max_val = my_list[0]
+    for num in my_list[1:]:
+        if num > max_val:
+            max_val = num
+    return max_val

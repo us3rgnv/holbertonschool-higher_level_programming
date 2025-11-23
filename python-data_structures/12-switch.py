6f5663240fb895d248287c112e2f3f100d903be1
us3rgnv <you@example.com>
2025-11-23 14:08:21 +0000

    Fix 12-switch.py to match 5-line requirement

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-a= 10
-b= 89
-a, b = b, a
-print("a={} - b={}".format(a, b))
+a = 89
+b = 10
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print("a={:d} - b={:d}".format(a, b))

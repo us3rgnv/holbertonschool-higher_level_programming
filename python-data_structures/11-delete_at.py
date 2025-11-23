8aeaac13d7f6c82a45f7261d313b1f55128023d5
us3rgnv <you@example.com>
2025-11-23 13:46:41 +0000

    Implement delete_at function that removes an item at a specific index

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    """Delete an item at a specific index in a list without using pop."""
+    """Delete an item at a specific index in a list in place."""
     if idx < 0 or idx >= len(my_list):
         return my_list
-    new_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
-    return new_list
+    for i in range(idx, len(my_list) - 1):
+        my_list[i] = my_list[i + 1]
+    del my_list[-1]
+    return my_list
+

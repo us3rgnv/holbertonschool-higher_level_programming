44c4705bcabafecb13881ae8f6e7eaab485db9da
us3rgnv <you@example.com>
2025-11-23 15:56:56 +0000

    Fix safe_print_list_integers to handle x > len(my_list) and non-integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x integers of a list."""
     count = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (ValueError, TypeError):
+        except (ValueError, TypeError, IndexError):
             continue
-        except IndexError:
-            break
     print()
     return count

2089d8df773855911612cd73ff00af4b100977de
us3rgnv <you@example.com>
2025-11-23 13:30:10 +0000

    Add function to print a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers, row by row."""
+    for row in matrix:
+        print(" ".join("{:d}".format(num) for num in row))

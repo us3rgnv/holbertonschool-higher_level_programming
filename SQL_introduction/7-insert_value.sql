083b55ab0c8c7992242d3c053fd678ec9fddcb1a
us3rgnv <your_email@example.com>
2025-11-30 09:23:00 +0000

    Add 7-insert_value.sql and README.md

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- 7-insert_value.sql
+-- This script inserts a new row into the table first_table
+-- id = 89, name = 'Best School'
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');

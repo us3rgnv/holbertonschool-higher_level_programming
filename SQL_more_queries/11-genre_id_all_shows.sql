c4ec53d218f7ae6fd18fbc05234b0e8cd7964072
us3rgnv <your_email@example.com>
2025-11-30 10:16:36 +0000

    Add script to list all TV shows with genre_id including NULLs

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,4 @@
--- 11-genre_id_all_shows.sql
--- List all shows with their genre IDs, NULL if no genre
-SELECT TV_SHOWS.TITLE, TV_SHOW_GENRES.GENRE_ID
-FROM TV_SHOWS
-LEFT JOIN TV_SHOW_GENRES
-ON TV_SHOWS.ID = TV_SHOW_GENRES.TV_SHOW_ID
-ORDER BY TV_SHOWS.TITLE ASC, TV_SHOW_GENRES.GENRE_ID ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

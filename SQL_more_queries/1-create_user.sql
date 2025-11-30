01327be0a5e3f0d0dc81c1e768ef91a00d9d4af7
us3rgnv <your_email@example.com>
2025-11-30 10:08:15 +0000

    Fix 1-create_user.sql with uppercase keywords and proper privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,5 @@
 -- 1-create_user.sql
--- This script creates the MySQL server user 'user_0d_1'
--- If the user already exists, the script will not fail
--- The user will have all privileges and the password will be 'user_0d_1_pwd'
-
+-- Create user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;

a56dd41437f486e93528cd239ac42aab60997b21
us3rgnv <you@example.com>
2025-12-07 17:02:44 +0000

    Add 10-my_github.py to get GitHub user ID via API

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script that takes your GitHub credentials and displays your id using the GitHub API"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    url = "https://api.github.com/user"
+
+    try:
+        response = requests.get(url, auth=(username, token))
+        if response.status_code == 200:
+            data = response.json()
+            print(data.get('id'))
+        else:
+            print(None)
+    except requests.exceptions.RequestException:
+        print(None)
